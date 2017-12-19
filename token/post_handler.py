09e59841ad6031ed03770503d3f9a0fbfdf29178
Brandon Davis <brandon@subdavis.com>
2017-12-19 08:56:36 -0500

    Don't return your hashes

--- token/post_handler.py
+++ token/post_handler.py
@@ -1,21 +1,37 @@
 import boto3
+import hashlib
 import uuid
+
 dynamodb = boto3.resource('dynamodb')
 
 def post(event, context):
-    table = dynamodb.Table('token')
+    # Gather what we need about the client.
+    # Never store ip or user agent in the clear to preserve my users' privacy
+    # TODO: salt these!
+    ip = event.requestContext.identity.sourceIp
+    ipHash = hashlib.sha224(ip).hexdigest()
+    
+    userAgent = event.requestContext.identity.userAgent
+    userAgentHash = hashlib.sha224(userAgent).hexdigest
 
-    item = {
+    tokenTable = dynamodb.Table('token')
+
+    tokenItem = {
         'id': str(uuid.uuid1()),
-        'foo': 'bar'
+        'ipHash': ipHash,
+        'ipHash_salt': "",
+        'userAgentHash': userAgentHash,
+        'userAgentHash_salt' : ""
     }
 
     # write the todo to the database
-    table.put_item(Item=item)
+    table.put_item(Item=tokenItem)
 
     response = {
         "statusCode": 200,
-        "body": "Iguess"
+        "body": {
+            'id': tokenItem.id,
+        }
     }
 
     return response
